8a39cfe4f06cde8a6abfae45934eec78664ed3f9
GoOz <gooz@foojin.com>
2026-03-31 22:00:02 +0200

    refactor: remove draft option & zod dependency

--- eleventy.config.js
+++ eleventy.config.js
@@ -17,12 +17,6 @@ import ExifReader from "exifreader"
 import svgContents from "eleventy-plugin-svg-contents"
 
 export default async function (eleventyConfig) {
-	// Drafts, see also _data/eleventyDataSchema.js
-	eleventyConfig.addPreprocessor("drafts", "*", (data, content) => {
-		if (data.draft && process.env.ELEVENTY_RUN_MODE === "build") {
-			return false
-		}
-	})
 	// PassThroughCopy
 	eleventyConfig.addPassthroughCopy({
 		"./public/": "/",
